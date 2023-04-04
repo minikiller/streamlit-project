975a326a35c9bb64518909d7d6010183000d62d2
Sun Lingfeng, Rise Sun <sunlingfeng70@gmail.com>
2023-04-04 15:12:52 +0800

    change

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 from pathlib import Path
 
 # Read the contents of the markdown file
-file_path = Path("./readme.md")
+file_path = Path("./README.md")
 markdown_text = file_path.read_text()
 st.markdown("# Main page 🎈")
 # Display the markdown text using st.markdown()
